580f4b476bc7d1ae2aa35d8c87419d0f2461640b
sostrovskyi <sostrovskyi>
2021-03-17 17:44:16 +0200

    move functions to API

--- tonsectl/app/tonseapi/tonseapi.go
+++ tonsectl/app/tonseapi/tonseapi.go
@@ -2,10 +2,23 @@ package tonseapi
 
 import (
     "fmt"
+    "os/exec"
     "log"
+    "compress/gzip"
     "net/http"
     //"github.com/gorilla/mux"
+    "io"
+    "os"
+    "archive/tar"
 )
+
+var tonosseUrl = "https://github.com/INTONNATION/tonos-se-installers/releases/download/tonos-se-v-0.25.0/"
+var tonosseTar = "tonos-se-v-0.25.0.tgz"
+var tonossePath = "/opt/tonsectl/"
+var tonosseConfigUrl = "https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/cfg"
+var tonosseLogCfg = "https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/log_cfg.yml"
+var tonossePrivKey = "https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/private-key"
+var tonossePubKey = "https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/pub-key"
 
 func tonseapi() {
     //myRouter := mux.NewRouter().StrictSlash(true)
@@ -23,6 +36,7 @@ func tonseInit(w http.ResponseWriter, r *http.Request){
 }
 
 func tonseStart(w http.ResponseWriter, r *http.Request){
+    node()
     fmt.Println("Endpoint Hit: tonseStart")
 }
 
@@ -40,6 +54,93 @@ func tonseReset(w http.ResponseWriter, r *http.Request){
 
 func tonseUpgrade(w http.ResponseWriter, r *http.Request){
     fmt.Println("Endpoint Hit: tonseUpgrade")
+}
+
+func downloadFile(filepath string, url string) (err error) {
+
+  out, err := os.Create(filepath)
+  if err != nil  {
+    return err
+  }
+  defer out.Close()
+
+  resp, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
+
+  if resp.StatusCode != http.StatusOK {
+    return fmt.Errorf("bad status: %s", resp.Status)
+  }
+
+  _, err = io.Copy(out, resp.Body)
+  if err != nil  {
+    return err
+  }
+
+  return nil
+}
+
+func extractTarGz(gzipStream io.Reader) {
+    uncompressedStream, err := gzip.NewReader(gzipStream)
+    if err != nil {
+        log.Fatal("extractTarGz: NewReader failed")
+    }
+
+    tarReader := tar.NewReader(uncompressedStream)
+
+    for true {
+        header, err := tarReader.Next()
+
+        if err == io.EOF {
+            break
+        }
+
+        if err != nil {
+            log.Fatalf("extractTarGz: Next() failed: %s", err.Error())
+        }
+
+        switch header.Typeflag {
+        case tar.TypeDir:
+            if err := os.Mkdir(header.Name, 0755); err != nil {
+                log.Fatalf("extractTarGz: Mkdir() failed: %s", err.Error())
+            }
+        case tar.TypeReg:
+            outFile, err := os.Create(tonossePath + header.Name)
+            if err != nil {
+                log.Fatalf("extractTarGz: Create() failed: %s", err.Error())
+            }
+            if _, err := io.Copy(outFile, tarReader); err != nil {
+                log.Fatalf("extractTarGz: Copy() failed: %s", err.Error())
+            }
+            outFile.Close()
+
+        default:
+            log.Fatalf(
+                "extractTarGz: uknown type: %s in %s",
+                header.Typeflag,
+                header.Name)
+        }
+    }
+}
+
+func node() {
+    //Commented because of private repo
+    //downloadFile(tonossePath+tonosseTar, tonosseUrl+tonosseTar)
+    tarFile, err1 := os.Open(tonossePath + tonosseTar)
+    if err1 != nil {
+        log.Fatalf(err1.Error())
+    }
+    extractTarGz(tarFile)
+    downloadFile(tonossePath+"cfg", tonosseConfigUrl)
+    downloadFile(tonossePath+"log_cfg.yml", tonosseLogCfg)
+    downloadFile(tonossePath+"private-key", tonossePrivKey)
+    downloadFile(tonossePath+"pub-key", tonossePubKey)
+    os.Chdir(tonossePath)
+    os.Chmod("ton_node_startup", 0700)
+    cmd := exec.Command("./ton_node_startup", "--config", "cfg")
+    cmd.Start()
 }
 
 func RunApi() {
