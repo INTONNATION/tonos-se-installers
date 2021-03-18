a2cc1f4d3a39e590106e5d95e06b70b5ad47d2a4
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2021-03-18 19:35:59 +0200

    working init command

--- tonsectl/cmd/init.go
+++ tonsectl/cmd/init.go
@@ -5,29 +5,37 @@ import (
     "github.com/spf13/cobra"
     "fmt"
     "os"
-
+    "strings"
     "os/exec"
 )
 
 func init() {
   rootCmd.AddCommand(initCmd)
+  rootCmd.AddCommand(apiCmd)
 }
 
 var initCmd = &cobra.Command{
     Use:   "init",
     Short: "Start TONSE API service",
     Run: func(cmd *cobra.Command, args []string) {
-       main()
+         api()
+    },
+}
+
+var apiCmd = &cobra.Command{
+    Use:   "api",
+    Short: "Start TONSE API service",
+    Run: func(cmd *cobra.Command, args []string) {
+         tonseapi.RunApi()
     },
 }
 
 
-
-
-func main() {
-    tonseapi.RunApi()
-    cmd := exec.Command(os.Args[1], "main")
-    cmd.Start()
-    fmt.Println("Daemon process ID is : ", cmd.Process.Pid)
+func api() {
+       if strings.ToLower(os.Args[1]) == "init" {
+           cmd := exec.Command(os.Args[0], "api")
+           cmd.Start()
+           fmt.Println("Daemon process ID is : ", cmd.Process.Pid)
+           os.Exit(0)
 }
-
+}
--- tonsectl/go.mod
+++ tonsectl/go.mod
@@ -1,18 +1,10 @@
 module github.com/INTONNATION/tonos-se-installers/tonsectl
 
 require (
-	github.com/99designs/gqlgen v0.12.2
-	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
-	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
-	github.com/cpuguy83/go-md2man v1.0.10 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
-	github.com/pkg/errors v0.9.1
+	github.com/gorilla/context v0.0.0-20160226214623-1ea25387ff6f // indirect
+	github.com/gorilla/mux v1.6.1
+	github.com/joho/godotenv v1.3.0
 	github.com/spf13/cobra v1.1.3
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/viper v1.7.0
-	github.com/ugorji/go v1.1.4 // indirect
-	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
-	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
 )
 
 go 1.16
