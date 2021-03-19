0ceb5daa78895e462f06f8ced07af6a1696e7075
sostrovskyi <sostrovskyi>
2021-03-19 21:40:35 +0200

    fix sh

--- tonsectl/init-scripts/init.linux.sh
+++ tonsectl/init-scripts/init.linux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-nodejs_version="16.16.0"
+nodejs_version="14.16.0"
 tonosse_version="0.25.0"
 arango_version="3.7.9"
+
+set -e
 
 mkdir -p ~/tonse
 tonossePath="$HOME/tonse"
@@ -10,7 +12,7 @@ tonossePath="$HOME/tonse"
 # Download tonosse and extract TON node and Graph binaries
 
 cd $tonossePath
-curl -O https://github.com/INTONNATION/tonos-se-installers/releases/download/tonos-se-v-$tonosse_version/tonos-se-v-$tonosse_version.tgz
+curl -LJO https://github.com/INTONNATION/tonos-se-installers/releases/download/tonos-se-v-$tonosse_version/tonos-se-v-$tonosse_version.tgz
 tar xf tonos-se-v-$tonosse_version.tgz
 rm tonos-se-v-$tonosse_version.tgz
 
@@ -20,12 +22,13 @@ curl -O https://download.arangodb.com/arangodb37/Community/Linux/arangodb3-linux
 tar xf arangodb3-linux-$arango_version.tar.gz
 rm arangodb3-linux-$arango_version.tar.gz
 
+rm $tonossePath/arangodb -rf
 mv arangodb3-linux-$arango_version $tonossePath/arangodb
 mkdir -p $tonossePath/arangodb/etc
 mkdir -p $tonossePath/arangodb/var/lib/arangodb3
 mkdir -p $tonossePath/arangodb/initdb.d/
 
-cp arangodb/config $tonossePath/arangodb/etc/config
+curl https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/arangodb/config -o $tonossePath/arangodb/etc/config
 curl https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/arango/initdb.d/upgrade-arango-db.js -o $tonossePath/arangodb/initdb.d/upgrade-arango-db.js
 
 # TON node
@@ -38,6 +41,7 @@ curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-nod
 curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/private-key
 curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/master/docker/ton-node/pub-key
 
+rm $tonossePath/node/ton_node_startup -f
 mv $tonossePath/ton_node_startup $tonossePath/node/
 chmod +x $tonossePath/node/ton_node_startup
 
@@ -49,7 +53,14 @@ cd $tonossePath/graphql
 curl -O https://nodejs.org/dist/v$nodejs_version/node-v$nodejs_version-linux-x64.tar.xz 
 tar -xf node-v$nodejs_version-linux-x64.tar.xz
 rm node-v$nodejs_version-linux-x64.tar.xz
+rm -rf ./nodejs
 mv ./node-v$nodejs_version-linux-x64/ ./nodejs
 qserver=`ls $tonossePath | grep ton-q-server`
+echo $qserver
+rm -rf $tonossePath/graphql/$qserver
 mv $tonossePath/$qserver $tonossePath/graphql/
-./nodejs/bin/npm install $qserver
+PATH=$PATH:$tonossePath/nodejs/bin/
+sudo mkdir /usr/lib/node_modules -p
+sudo chown -R $USER /usr/lib/node_modules
+./nodejs/bin/npm install $qserver --production -g
+rm -rf $tonossePath/graphql/$qserver 
