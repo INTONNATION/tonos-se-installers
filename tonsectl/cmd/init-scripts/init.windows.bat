2822cc10349c15dc6026ced5470e6cbbb8ce7322
Your Name <you@example.com>
2021-05-31 15:13:47 +0300

    Add installation updates for Windows

--- tonsectl/cmd/init-scripts/init.windows.bat
+++ tonsectl/cmd/init-scripts/init.windows.bat
@@ -11,39 +11,39 @@ SET tonossePath=%userprofile%\tonse
 mkdir %tonossePath%
 mkdir %tonossePath%\caddy
 cd %tonossePath%\caddy
-curl -O https://github.com/caddyserver/caddy/releases/download/v2.4.0-beta.2/caddy_2.4.0-beta.2_windows_amd64.zip
+curl -s -L -O https://github.com/caddyserver/caddy/releases/download/v2.4.0-beta.2/caddy_2.4.0-beta.2_windows_amd64.zip
 tar xf caddy_2.4.0-beta.2_windows_amd64.zip
-curl -O https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/caddy/Caddyfile
+curl -s -L -O https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/caddy/Caddyfile
 
 :: Release downloading
 cd %tonossePath%
-curl -LJO https://github.com/INTONNATION/tonos-se-installers/releases/download/%tonosse_version%/tonos-se-windows.tar
+curl -s -LJO https://github.com/INTONNATION/tonos-se-installers/releases/download/%tonosse_version%/tonos-se-windows.tar
 tar xf tonos-se-windows.tar
-move tonos-se/docker/ton-live/web/ $tonossePath/caddy/web/
+move tonos-se\docker\ton-live\web %tonossePath%\caddy\web
 DEL /Q tonos-se-windows.tar
 
 :: ArangoDB
 
-curl -O https://download.arangodb.com/arangodb37/Community/Windows/ArangoDB3-%arango_version%_win64.zip
+curl -s -O https://download.arangodb.com/arangodb37/Community/Windows/ArangoDB3-%arango_version%_win64.zip
 tar xf ArangoDB3-%arango_version%_win64.zip
 DEL /Q ArangoDB3-%arango_version%_win64.zip
 move ArangoDB3-%arango_version%_win64 %tonossePath%\arangodb
 mkdir %tonossePath%\arangodb\etc
 mkdir %tonossePath%\arangodb\var\lib\arangodb3
 mkdir %tonossePath%\arangodb\initdb.d
-curl https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/arangodb/config -o %tonossePath%\arangodb\etc\config
-curl https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/arango/initdb.d/upgrade-arango-db.js -o %tonossePath%\arangodb\initdb.d\upgrade-arango-db.js
+curl -s https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/arangodb/config -o %tonossePath%\arangodb\etc\config
+curl -s https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/arango/initdb.d/upgrade-arango-db.js -o %tonossePath%\arangodb\initdb.d\upgrade-arango-db.js
 
 :: TON node
 
 mkdir %tonossePath%\node
 cd %tonossePath%\node
 
-curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/$tonosse_version/docker/ton-node/blockchain.conf.json
-curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/$tonosse_version/docker/ton-node/ton-node.conf.json
-curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/log_cfg.yml
-curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/private-key
-curl -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/pub-key
+curl -s -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/blockchain.conf.json
+curl -s -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/ton-node.conf.json
+curl -s -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/log_cfg.yml
+curl -s -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/private-key
+curl -s -O https://raw.githubusercontent.com/tonlabs/tonos-se/%tonosse_version%/docker/ton-node/pub-key
 
 move %tonossePath%\ton_node_startup.exe %tonossePath%\node\ton_node_startup.exe
 
@@ -51,7 +51,7 @@ move %tonossePath%\ton_node_startup.exe %tonossePath%\node\ton_node_startup.exe
 
 mkdir %tonossePath%\graphql
 cd %tonossePath%\graphql
-curl -O https://nodejs.org/dist/v%nodejs_version%/node-v%nodejs_version%-win-x64.zip
+curl -s -O https://nodejs.org/dist/v%nodejs_version%/node-v%nodejs_version%-win-x64.zip
 tar xf node-v%nodejs_version%-win-x64.zip
 del /Q node-v%nodejs_version%-win-x64.zip
 move node-v%nodejs_version%-win-x64 nodejs
@@ -61,7 +61,7 @@ move %tonossePath%\%qserver% %tonossePath%\graphql\
 set PATH=%PATH%;%tonossePath%\graphql\nodejs
 
 tar xf %qserver%
-curl -o %tonossePath%\graphql\package\.env https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/graphql/.env
+curl -s -o %tonossePath%\graphql\package\.env https://raw.githubusercontent.com/INTONNATION/tonos-se-installers/master/tonsectl/graphql/.env
 
 npm install %qserver% --production
 del /Q /S %qserver%
