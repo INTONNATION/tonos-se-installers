f0b64c6c17a853c6222f38cb1b59d8d9e8b76d7b
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2021-08-08 22:57:35 +0300

    rebuild 0.28.6

--- tonsectl/cmd/version.go
+++ tonsectl/cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
   Short: "Print the version number of tonos_installer",
   Long:  `All software has versions. This is tonos_installer`,
   Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("v0.28.5")
+    fmt.Println("v0.28.6")
   },
 }
